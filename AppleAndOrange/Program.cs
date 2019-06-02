70c9a0da4b170706689638233308c84b13793061
yordanatanasovv <atanasovyordan321@gmail.com>
2019-06-02 21:39:51 +0300

    Added 2 solutions from implementation section.

---
+++ AppleAndOrange/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Linq;
+
+namespace AppleAndOrange
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] houseRange = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            int s = houseRange[0];
+            int t = houseRange[1];
+
+            int[] treeValues = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            int a = treeValues[0];
+            int b = treeValues[1];
+
+            int[] fruitsPerTree = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            int m = fruitsPerTree[0];
+            int n = fruitsPerTree[1];
+
+            int[] apples = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            int[] oranges = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            CountApplesAndOranges(s, t, a, b, apples, oranges);
+        }
+
+        public static void CountApplesAndOranges(int s, int t, int a, int b, int[] apples, int[] oranges)
+        {
+            int[] applesFinal = new int[apples.Length];
+            int[] orangesFinal = new int[oranges.Length];
+
+            int applesCount = 0;
+            int orangesCount = 0;
+
+            for (int i = 0; i < apples.Length; i++)
+            {
+                applesFinal[i] = a + apples[i];
+            }
+
+            for (int i = 0; i < oranges.Length; i++)
+            {
+                orangesFinal[i] = b + oranges[i];
+            }
+
+            for (int i = 0; i < applesFinal.Length; i++)
+            {
+                if (applesFinal[i] >= s && applesFinal[i] <= t)
+                {
+                    applesCount++;
+                }
+            }
+
+            for (int i = 0; i < orangesFinal.Length; i++)
+            {
+                if (orangesFinal[i] >= s && orangesFinal[i] <= t)
+                {
+                    orangesCount++;
+                }
+            }
+
+            Console.WriteLine(applesCount);
+            Console.WriteLine(orangesCount);
+
+        }
+    }
+}
---
+++ Kangoroo/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Linq;
+
+namespace Kangoroo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] kangorooArgs = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            int x1 = kangorooArgs[0];
+            int v1 = kangorooArgs[1];
+            int x2 = kangorooArgs[2];
+            int v2 = kangorooArgs[3];
+
+            Console.WriteLine(Kangaroo(x1,v1,x2,v2));
+
+        }
+
+        public static string Kangaroo(int x1, int v1, int x2, int v2)
+        {
+            if (x1 < x2 && v1 < v2)
+            {
+                return "NO";
+            }
+            else if (x1 < 0 || x2 < 0)
+            {
+                return "NO";
+            }
+            else if (v1 <  1 || v2 < 1)
+            {
+                return "NO";
+            }
+            else
+            {
+                for (int i = 0; i <= 10000; i++)
+                {
+                    x1 += v1;
+                    x2 += v2;
+
+                    if (x1 == x2)
+                    {
+                        return "YES";
+                    }   
+                }               
+            }
+
+            return "NO";
+
+        }
+    }
+}
