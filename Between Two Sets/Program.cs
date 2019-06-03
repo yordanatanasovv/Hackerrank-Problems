4590eba69aa7ed2b8f2363cda0caaa1c0c11a48b
yordanatanasovv <atanasovyordan321@gmail.com>
2019-06-03 23:03:07 +0300

    1 solution from implemented section.

---
+++ Between Two Sets/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Between_Two_Sets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arrayCounts = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            int[] firstArray = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            int[] secondArray = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            Console.WriteLine(getTotalX(firstArray,secondArray));
+        }
+
+        public static int getTotalX(int[] a, int[] b)
+        {
+            List<int> validNumbers = new List<int>();
+            List<int> validNumberAfterScond = new List<int>();
+            int count = 0;
+
+            for (int i = a.Max(); i <= b.Min(); i++)
+            {
+                if (IsValidFirstArrayNumber(i, a))
+                {
+                    validNumbers.Add(i);
+                }
+            }
+
+            for (int i = 0; i < validNumbers.Count; i++)
+            {
+                if (IsValidSecondArrayNumber(validNumbers[i], b))
+                {
+                    count++;
+                    validNumberAfterScond.Add(validNumbers[i]);
+                }
+            }
+
+            return count;
+        }
+
+        public static bool IsValidFirstArrayNumber(int number, int[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (number % array[i] != 0)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        public static bool IsValidSecondArrayNumber(int number, int[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] % number != 0)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
