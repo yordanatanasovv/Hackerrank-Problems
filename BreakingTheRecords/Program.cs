ed0e58d1eb82b9a82597b127a70280f7be3a0605
yordanatanasovv <atanasovyordan321@gmail.com>
2019-09-30 18:38:30 +0300

    Added 2 new solutions from implementation section.

---
+++ BreakingTheRecords/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BreakingTheRecords
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            string points = Console.ReadLine();
+
+            int[] scores = points
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            scores = BreakingRecords(scores);
+
+            Console.WriteLine(string.Join(" ", scores));
+
+        }
+
+        public static int[] BreakingRecords(int[] scores)
+        {
+            int highestScore = scores[0];
+            int highestScoreCount = 0;
+            int lowestScoreCount = 0;
+            int lowestScore = scores[0];
+
+            for (int i = 1; i < scores.Length; i++)
+            {
+                if (scores[i] > highestScore)
+                {
+                    highestScore = scores[i];
+                    highestScoreCount++;
+                }
+                else if (scores[i] < lowestScore)
+                {
+                    lowestScore = scores[i];
+                    lowestScoreCount++;
+                }
+            }
+
+            int[] result = new int[]
+            {
+                highestScoreCount,
+                lowestScoreCount
+            };
+
+            return result;
+        }
+    }
+}
---
+++ GradingStudents/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GradingStudents
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            List<int> grades = new List<int>();
+
+            for (int i = 0; i < n; i++)
+            {
+                int num = int.Parse(Console.ReadLine());
+                grades.Add(num);
+            }
+
+            GradingStudents(grades);
+
+            for (int i = 0; i < grades.Count; i++)
+            {
+                Console.WriteLine(grades[i]);
+            }
+
+
+        }
+
+        public static List<int> GradingStudents(List<int> grades)
+        {
+            for (int i = 0; i < grades.Count; i++)
+            {
+                int roundedNumber = grades[i] % 10;
+                int difference;
+
+                if (grades[i] >= 38)
+                {
+                    if (roundedNumber <= 5)
+                    {
+                        difference = 5 - roundedNumber;
+
+                        if (difference < 3)
+                        {
+                            grades[i] += difference;
+                        }
+                    }
+                    else if (roundedNumber > 5 && roundedNumber <= 10)
+                    {
+                        difference = 10 - roundedNumber;
+
+                        if (difference < 3)
+                        {
+                            grades[i] += difference;
+                        }
+                    }
+                }
+            }
+
+            return grades;
+        }
+    }
+}
